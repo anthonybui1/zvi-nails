5714dcb521de1303a6ba031bbb484b8c59d1e543
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-05 16:49:47 -0600

    Tailwind configuration added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primpink: '#FFB5A7',
+				lightpink: '#FCD5CE',
+				neutral: '#F8EDEB',
+				skin: '#F9DCC4',
+				tan: '#FEC89A',
+			},
+			fontFamily: {
+				display: ['Playfair Display', 'serif'],
+				header: ['Lustria', 'serif'],
+				body: ['Amethysta', 'serif'],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
