83bc3d65e5599531797c0e2092a0bc3f8c41eea6
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-08 16:09:37 -0600

    Hopefully fixed image sizing issue on Mobile Safari

--- src/organisms/About.js
+++ src/organisms/About.js
@@ -4,13 +4,11 @@ import familyImg from '../assets/family.jpg';
 const About = () => {
 	return (
 		<div className='flex my-4' id='about'>
-			<img
-				className='xl:w-144 lg:w-96 md:w-72 w-40'
-				src={familyImg}
-				alt='Person holding bouquet.'
-			/>
+			<div className='w-1/2 flex-shrink-0'>
+				<img className='object-fit' src={familyImg} alt='Person holding bouquet.' />
+			</div>
 			<div className='bg-lightpink w-full flex flex-col justify-center items-center px-4'>
-				<h1 className='font-header text-lg md:text-3xl mb-2'>
+				<h1 className='font-header text-lg md:text-4xl mb-2'>
 					<strong>About Us</strong>
 				</h1>
 				<p className='md:text-base text-xs text-center'>
--- src/organisms/Services.js
+++ src/organisms/Services.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 const Services = () => {
 	return (
 		<div className='flex flex-col bg-neutral justify-center items-center px-8 w-full' id='services'>
-			<h1 className='font-header text-3xl mt-6 mb-2'>
+			<h1 className='font-header md:text-4xl text-3xl mt-6 mb-2'>
 				<strong>Services</strong>
 			</h1>
-			<ul className='text-center'>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Nail Services</li>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Shellac Nails</li>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Kids Packages</li>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Pedicures</li>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Manicures</li>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Waxing & Threading</li>
-				<li className='hover:bg-lightpink w-full px-2 py-2'>Facials</li>
+			<ul className='text-center md:text-lg'>
+				<li className='w-full px-2 py-2'>Nail Services</li>
+				<li className='w-full px-2 py-2'>Shellac Nails</li>
+				<li className='w-full px-2 py-2'>Kids Packages</li>
+				<li className='w-full px-2 py-2'>Pedicures</li>
+				<li className='w-full px-2 py-2'>Manicures</li>
+				<li className='w-full px-2 py-2'>Waxing & Threading</li>
+				<li className='w-full px-2 py-2'>Facials</li>
 			</ul>
 			<Link
 				to='/services'
-				className='my-4 py-2 px-4 shadow-md hover:shadow-lg border-4 border-primpink cursor-pointer'
+				className='my-4 py-2 px-4 shadow-md hover:shadow-lg border-4 border-primpink cursor-pointer md:text-lg'
 			>
-				Menu
+				<strong>Menu</strong>
 			</Link>
 		</div>
 	);
