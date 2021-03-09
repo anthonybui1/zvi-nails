f8adeaba86793e03d5957daf729120d97bc05b4e
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-08 19:30:36 -0600

    Trying to fix scroll issue on mobile safari

--- src/molecules/MobileMenu.js
+++ src/molecules/MobileMenu.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import MobileMenuAnchor from './MobileMenuAnchor';
 import { Link } from 'react-router-dom';
 
 const MobileMenu = ({ setOpen, isOpen }) => {
-	const handleHomeClick = () => {
+	const handleClick = (location) => {
 		setOpen(false);
-		window.scrollTo(0, 0);
+		setTimeout(() => {
+			document.getElementById(location).scrollIntoView();
+		}, 500);
 	};
 
 	return (
 		<ul
-			className={`fixed top-0 left-0 z-10 w-full h-screen bg-lightpink transition-all duration-300 ease-out transform flex flex-col justify-center items-center ${
+			className={`fixed top-0 left-0 z-10 w-full h-screen bg-lightpink transition-all duration-500 ease-out transform flex flex-col justify-center items-center ${
 				isOpen ? 'opacity-90 translate-y-0' : 'opacity-0 -translate-y-full'
 			}`}
 		>
 			<Link
 				to='/'
-				onClick={handleHomeClick}
+				onClick={() => handleClick('root')}
 				className='mt-8 text-xl p-2 border-b-4 border-primpink'
 			>
 				Home
 			</Link>
-			<MobileMenuAnchor title='Gallery' link='#gallery' setOpen={setOpen} />
+			<Link
+				to='/'
+				onClick={() => handleClick('gallery')}
+				className='mt-8 text-xl p-2 border-b-4 border-primpink'
+			>
+				Gallery
+			</Link>
 			<Link
 				to='/services'
 				onClick={() => setOpen(false)}
@@ -29,8 +36,20 @@ const MobileMenu = ({ setOpen, isOpen }) => {
 			>
 				Services
 			</Link>
-			<MobileMenuAnchor title='About' link='#about' setOpen={setOpen} />
-			<MobileMenuAnchor title='Contact' link='#contact' setOpen={setOpen} />
+			<Link
+				to='/'
+				onClick={() => handleClick('about')}
+				className='mt-8 text-xl p-2 border-b-4 border-primpink'
+			>
+				About
+			</Link>
+			<Link
+				to='/'
+				onClick={() => handleClick('contact')}
+				className='mt-8 text-xl p-2 border-b-4 border-primpink'
+			>
+				Contact
+			</Link>
 		</ul>
 	);
 };
