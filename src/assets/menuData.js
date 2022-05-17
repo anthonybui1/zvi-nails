5296a15f0dd197be3f08cce209361c2023062da4
Anthony Bui <nhut.anthony.bui@gmail.com>
2022-05-16 23:10:32 -0500

    chore: Menu pricing updates

--- src/assets/menuData.js
+++ src/assets/menuData.js
@@ -1,109 +1,110 @@
 export const menuData = {
-	nailServices: [
-		{ name: 'Full Set Pink Solar w/ Regular Polish', price: '$35+' },
-		{ name: 'Full Set Solar Color Powder', price: '$43+' },
-		{ name: 'Full Set Pink & White', price: '$48+' },
-		{ name: 'Full Set Pink Solar w/ Shellac', price: '$48+' },
-		{ name: 'Refill Pink', price: '$28+' },
-		{ name: 'Refill Same Color', price: '$33+' },
-		{ name: 'Refill Color Powder', price: '$38+' },
-		{ name: 'Refill Pink & White', price: '$38+' },
-		{ name: 'Refill w/ Shellac', price: '$38+' },
-		{ name: 'Nail Dip', price: '$43+' },
-		{ name: 'Dip French', price: '$48+' },
-	],
-	shellacNails: [
-		{ name: 'Manicure Shellac', price: '$30+' },
-		{ name: 'French Manicure Shellac', price: '$35+' },
-		{ name: 'Shellac Hand Polish Change', price: '$20+' },
-		{ name: 'Shellac Toes Polish Change', price: '$23+' },
-	],
-	kidsPackages: [
-		{ name: 'Mani + Pedi', price: '$28+' },
-		{ name: 'Hand Polish Change', price: '$5+' },
-		{ name: 'Toes Polish Change', price: '$7+' },
-	],
-	pedicures: [
-		{
-			name: 'Regular Pedicure',
-			price: '$27',
-			description:
-				'Soak your feet in our warm spa to improve blood circulation while we perform full nail maintenance (trimming, shape, and cuticle details). Follow with Sugar Scrub to exfoliate all your dry skin cells, callus removal, and a relaxing Massage with moisturizing lotion. Finish with relaxing warm towel therapy and expert polish application.',
-		},
-		{
-			name: 'Deluxe Pedicure',
-			price: '$38',
-			description:
-				'Have your own choice with our 4/1 deluxe spa pedicure. Options include Pedi Tablet with Sugar Scrub, Mud Masque, or Lotion. Full nail maintenance is provided. Hot stone massage also comes along with the Pedi Tablet. Finish with an expert application of color polish.',
-		},
-		{
-			name: 'Collagen Pedicure',
-			price: '$53',
-			description:
-				'Your feet will soak in our aromatic spa while we add a packet of Collagen Bubble Crystals to the water and perform full nail maintenance. Next we apply Collagen Sugar Cane Scrub (your choice) and massage your skin to exfoliate for a more vibrant appearance. We follow with the addition  of a Collagen Cream Mask wrapped in a warm towel to rejuvenate and soften your skin. Collagen Muscle — Relaxing Gel and Massage Lotion is applied onto your skin for a warm soothing effect and silky finish followed by wax and hot stone massage. Finish with color polish of your choice. ',
-		},
-		{
-			name: 'Volcano Pearl Pedicure',
-			price: '$63',
-			description:
-				'The Volcano Spa Pedicure is a luxury 5 step organic package from the deep sea, formulated to provide clients a premium pedicure experience. Detox crystals and activators combine to create an invigorating and detoxifying experience. Pearl powder promotes cell renewal for youthful and healthy-looking skin. Natural moisturizer protects from UV rays and free radicals. Soaking your feet in a warm bath of pearl soak gives you a stress clearing experience. Next is full nail maintenance followed by a Pearl Cleanse which will then be applied to exfoliate all impurities, dry skin and rough heels. A creamy pearl mask wrapped in a warm towel is applied to condition and hydrate your skin. We finish with hot paraffin wax and hot stone massage along with expert application of color polish.',
-		},
-	],
-	manicures: [
-		{
-			name: 'Regular Manicure',
-			price: '$15',
-			description:
-				'Soak your hands in our warm bath while we perform nail trimming, shaping, cuticle care and provide a relaxing massage with special essential oils that relieve your achey hands. Apply a color of your choice or allow us to buffer your nails to a clean shiny appearance.',
-		},
-		{
-			name: 'Deluxe Manicure',
-			price: '$30',
-			description:
-				'Aromatic soaking of hands in the our warm crystal water bath with while we perform full manicure maintenance to exfoliate dry, dead skin cells with a Sugar Scrub (your choice), and a mask wrapped in a warm towel to hydrate and condition your skin for a healthy look. Paraffin hot wax is applied while you are pampered with the hot stone massage. End with an expert application of color polish.',
-		},
-	],
-	waxingAndThreading: [
-		{ name: 'Eyebrow Wax', price: '$8+' },
-		{ name: 'Eyebrow Threading', price: '$10+' },
-		{ name: 'Upper Lip Wax', price: '$6+' },
-		{ name: 'Upper Lip Threading', price: '$7+' },
-		{ name: 'Chin Wax', price: '$6+' },
-		{ name: 'Chin Threading', price: '$7+' },
-		{ name: 'Sideburns', price: '$15+' },
-		{ name: 'Full Face Wax', price: '$30+' },
-		{ name: 'Full Face Threading', price: '$37+' },
-		{ name: 'Chest', price: '$30+' },
-		{ name: 'Back', price: '$50+' },
-		{ name: 'Full Arms', price: '$40+' },
-		{ name: 'Lower Arms', price: '$25+' },
-		{ name: 'Under Arms', price: '$25+' },
-		{ name: 'Brazilian', price: '$50+' },
-		{ name: 'Bikini', price: '$40+' },
-		{ name: 'Full Legs', price: '$45+' },
-		{ name: 'Lower Legs', price: '$35+' },
-	],
-	facials: [
-		{
-			name: 'ageLOC Refreshing Facial',
-			price: '$55',
-			duration: '60 min',
-			description:
-				"Smoothes the skin's surface by releasing dead cell buildup and pore-blocking impurities. Cleanses and purifies your skin, increases hydration, delivers your first infusion of ageLOC to reduce the signs of aging. Steams skin for blackhead removal. Extracts impurities, removes dead skin cells, and rejuvenates damaged skin. Infuses soothing nutrients from cactus extract to help keep skin healthy-looking, beautiful, and fully moisturized. Leaving the skin smooth and feeling refreshed!",
-		},
-		{
-			name: 'ageLOC Anti-Aging Facial',
-			price: '$65',
-			duration: '70 min',
-			description:
-				'Includes all steps from ageLOC Refreshing Facial, also indulge in a luxurious 10 minute spa experience for a more radiant, youthful face. Works immediately to lift, firm and tone your skin while reducing the appearance of lines and wrinkles. Leave undefined skin in the past and enjoy firmer, younger looking skin today and in the future, with ageLOC Tru Face Essence Ultra.',
-		},
-		{
-			name: 'Refresh Mask',
-			price: '$25',
-			duration: '',
-			description:
-				'Pamper your skin with an exfoliating treatment and a refreshing face mask that will leave your skin feeling moisturized. Add on this special Mask anytime while you do your Nails or any Mani + Pedi Spa Service',
-		},
-	],
+  nailServices: [
+    { name: 'Full Set Pink Solar w/ Regular Polish', price: '$35+' },
+    { name: 'Full Set Solar Color Powder', price: '$43+' },
+    { name: 'Full Set Pink & White', price: '$48+' },
+    { name: 'Full Set Pink Solar w/ Shellac', price: '$48+' },
+    { name: 'Refill Pink', price: '$28+' },
+    { name: 'Refill Same Color', price: '$33+' },
+    { name: 'Refill Color Powder', price: '$38+' },
+    { name: 'Refill Pink & White', price: '$38+' },
+    { name: 'Refill w/ Shellac', price: '$38+' },
+    { name: 'Nail Dip', price: '$43+' },
+    { name: 'Dip French', price: '$48+' },
+  ],
+  shellacNails: [
+    { name: 'Manicure Shellac', price: '$30+' },
+    { name: 'French Manicure Shellac', price: '$35+' },
+    { name: 'Shellac Hand Polish Change', price: '$20+' },
+    { name: 'Shellac Toes Polish Change', price: '$23+' },
+  ],
+  kidsPackages: [
+    { name: 'Mani + Pedi', price: '$28+' },
+    { name: 'Hand Polish Change', price: '$5+' },
+    { name: 'Toes Polish Change', price: '$7+' },
+  ],
+  pedicures: [
+    {
+      name: 'Regular Pedicure',
+      price: '$27',
+      description:
+        'Soak your feet in our warm spa to improve blood circulation while we perform full nail maintenance (trimming, shape, and cuticle details). Follow with Sugar Scrub to exfoliate all your dry skin cells, callus removal, and a relaxing Massage with moisturizing lotion. Finish with relaxing warm towel therapy and expert polish application.',
+    },
+    {
+      name: 'Deluxe Pedicure',
+      price: '$38',
+      description:
+        'Have your own choice with our 4/1 deluxe spa pedicure. Options include Pedi Tablet with Sugar Scrub, Mud Masque, or Lotion. Full nail maintenance is provided. Hot stone massage also comes along with the Pedi Tablet. Finish with an expert application of color polish.',
+      addition: '(with Wax and Extra Massage $45)',
+    },
+    {
+      name: 'Collagen Pedicure',
+      price: '$53',
+      description:
+        'Your feet will soak in our aromatic spa while we add a packet of Collagen Bubble Crystals to the water and perform full nail maintenance. Next we apply Collagen Sugar Cane Scrub (your choice) and massage your skin to exfoliate for a more vibrant appearance. We follow with the addition  of a Collagen Cream Mask wrapped in a warm towel to rejuvenate and soften your skin. Collagen Muscle — Relaxing Gel and Massage Lotion is applied onto your skin for a warm soothing effect and silky finish followed by wax and hot stone massage. Finish with color polish of your choice. ',
+    },
+    {
+      name: 'Volcano Pearl Pedicure',
+      price: '$63',
+      description:
+        'The Volcano Spa Pedicure is a luxury 5 step organic package from the deep sea, formulated to provide clients a premium pedicure experience. Detox crystals and activators combine to create an invigorating and detoxifying experience. Pearl powder promotes cell renewal for youthful and healthy-looking skin. Natural moisturizer protects from UV rays and free radicals. Soaking your feet in a warm bath of pearl soak gives you a stress clearing experience. Next is full nail maintenance followed by a Pearl Cleanse which will then be applied to exfoliate all impurities, dry skin and rough heels. A creamy pearl mask wrapped in a warm towel is applied to condition and hydrate your skin. We finish with hot paraffin wax and hot stone massage along with expert application of color polish.',
+    },
+  ],
+  manicures: [
+    {
+      name: 'Regular Manicure',
+      price: '$15',
+      description:
+        'Soak your hands in our warm bath while we perform nail trimming, shaping, cuticle care and provide a relaxing massage with special essential oils that relieve your achey hands. Apply a color of your choice or allow us to buffer your nails to a clean shiny appearance.',
+    },
+    {
+      name: 'Deluxe Manicure',
+      price: '$30',
+      description:
+        'Aromatic soaking of hands in the our warm crystal water bath with while we perform full manicure maintenance to exfoliate dry, dead skin cells with a Sugar Scrub (your choice), and a mask wrapped in a warm towel to hydrate and condition your skin for a healthy look. Paraffin hot wax is applied while you are pampered with the hot stone massage. End with an expert application of color polish.',
+    },
+  ],
+  waxingAndThreading: [
+    { name: 'Eyebrow Wax', price: '$10+' },
+    { name: 'Eyebrow Threading', price: '$10+' },
+    { name: 'Upper Lip Wax', price: '$6+' },
+    { name: 'Upper Lip Threading', price: '$7+' },
+    { name: 'Chin Wax', price: '$6+' },
+    { name: 'Chin Threading', price: '$7+' },
+    { name: 'Sideburns', price: '$15+' },
+    { name: 'Full Face Wax', price: '$30+' },
+    { name: 'Full Face Threading', price: '$37+' },
+    { name: 'Chest', price: '$30+' },
+    { name: 'Back', price: '$50+' },
+    { name: 'Full Arms', price: '$40+' },
+    { name: 'Lower Arms', price: '$25+' },
+    { name: 'Under Arms', price: '$25+' },
+    { name: 'Brazilian', price: '$50+' },
+    { name: 'Bikini', price: '$40+' },
+    { name: 'Full Legs', price: '$45+' },
+    { name: 'Lower Legs', price: '$35+' },
+  ],
+  facials: [
+    {
+      name: 'ageLOC Refreshing Facial',
+      price: '$55',
+      duration: '60 min',
+      description:
+        "Smoothes the skin's surface by releasing dead cell buildup and pore-blocking impurities. Cleanses and purifies your skin, increases hydration, delivers your first infusion of ageLOC to reduce the signs of aging. Steams skin for blackhead removal. Extracts impurities, removes dead skin cells, and rejuvenates damaged skin. Infuses soothing nutrients from cactus extract to help keep skin healthy-looking, beautiful, and fully moisturized. Leaving the skin smooth and feeling refreshed!",
+    },
+    {
+      name: 'ageLOC Anti-Aging Facial',
+      price: '$65',
+      duration: '70 min',
+      description:
+        'Includes all steps from ageLOC Refreshing Facial, also indulge in a luxurious 10 minute spa experience for a more radiant, youthful face. Works immediately to lift, firm and tone your skin while reducing the appearance of lines and wrinkles. Leave undefined skin in the past and enjoy firmer, younger looking skin today and in the future, with ageLOC Tru Face Essence Ultra.',
+    },
+    {
+      name: 'Refresh Mask',
+      price: '$25',
+      duration: '',
+      description:
+        'Pamper your skin with an exfoliating treatment and a refreshing face mask that will leave your skin feeling moisturized. Add on this special Mask anytime while you do your Nails or any Mani + Pedi Spa Service',
+    },
+  ],
 };
--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -6,20 +6,19 @@ const Menu = () => {
 	return (
 		<section className='mx-auto max-w-prose flex flex-col mt-8 mb-24 px-4'>
 			<img className="mb-8" alt="Manicurist at work" src={manicuristWorking} />
-
 			<article>
 				<h2 className='font-display text-3xl text-center font-bold'>Pedicures</h2>
 				{menuData.pedicures.map((item, index) => {
 					return (
 						<React.Fragment key={index}>
 							<h3 className='font-display text-2xl mt-4'>{item.name}</h3>
+							<p>{item.addition}</p>
 							<p>{item.description}</p>
 							<p className="text-lg text-right">{item.price}</p>
 						</React.Fragment>
 					);
 				})}
 			</article>
-
 			<article>
 				<h2 className='font-display text-3xl text-center mt-4 font-bold'>Manicures</h2>
 				{menuData.manicures.map((item, index) => {
@@ -32,7 +31,6 @@ const Menu = () => {
 					);
 				})}
 			</article>
-
 			<article>
 				<h2 className='font-display text-3xl text-center mt-4 font-bold'>Facials</h2>
 				{menuData.facials.map((item, index) => {
@@ -45,7 +43,6 @@ const Menu = () => {
 					);
 				})}
 			</article>
-
 			<article className='space-y-2'>
 				<h2 className='font-display text-3xl text-center my-4 font-bold'>Nail Services</h2>
 				{menuData.nailServices.map((item, index) => {
@@ -57,7 +54,6 @@ const Menu = () => {
 					);
 				})}
 			</article>
-
 			<article className='space-y-2'>
 				<h2 className='font-display text-3xl text-center my-4 font-bold'>Waxing & Threading</h2>
 				{menuData.waxingAndThreading.map((item, index) => {
@@ -69,7 +65,6 @@ const Menu = () => {
 					);
 				})}
 			</article>
-
 			<article className='space-y-2'>
 				<h2 className='font-display text-3xl text-center my-4 font-bold'>Shellac Nails</h2>
 				{menuData.shellacNails.map((item, index) => {
@@ -81,7 +76,6 @@ const Menu = () => {
 					);
 				})}
 			</article>
-
 			<article className='space-y-2'>
 				<h2 className='font-display text-3xl text-center my-4 font-bold'>Kids Packages</h2>
 				{menuData.kidsPackages.map((item, index) => {
